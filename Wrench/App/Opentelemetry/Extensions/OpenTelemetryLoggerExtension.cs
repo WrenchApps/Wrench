ce4153110fb46b9907a588d853003cf446ed5405
Ivan Meireles <ivanmds2@hotmail.com>
2024-06-06 22:05:17 -0300

    add otel  to logs

--- Wrench/App/Opentelemetry/Extensions/OpenTelemetryLoggerExtension.cs
+++ Wrench/App/Opentelemetry/Extensions/OpenTelemetryLoggerExtension.cs
@@ -1,12 +1,12 @@
-﻿//using System;
-//using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using Opentelemetry.Configuration;
-//using Microsoft.Extensions.Logging;
-//using OpenTelemetry.Exporter;
-//using OpenTelemetry.Logs;
-//using OpenTelemetry.Metrics;
-//using OpenTelemetry.Resources;
-//using OpenTelemetry.Trace;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace Opentelemetry.Extensions
 {
@@ -14,24 +14,24 @@ namespace Opentelemetry.Extensions
     {
         public static ILoggingBuilder AddOpenTelemetryLogging(this ILoggingBuilder builder, OpenTelemetryConfig config)
         {
-            //var protocol = config.IsGrpc ? OtlpExportProtocol.Grpc : OtlpExportProtocol.HttpProtobuf;
-            //var endpoint = protocol == OtlpExportProtocol.Grpc ? new Uri(config.Endpoint) : new Uri($"{config.Endpoint}/v1/logs");
+            var protocol = config.IsGrpc ? OtlpExportProtocol.Grpc : OtlpExportProtocol.HttpProtobuf;
+            var endpoint = protocol == OtlpExportProtocol.Grpc ? new Uri(config.Endpoint) : new Uri($"{config.Endpoint}/v1/logs");
 
-            //builder.AddOpenTelemetry((OpenTelemetryLoggerOptions loggingbuilder) =>
-            //{
-            //    loggingbuilder.AddOtlpExporter(opt =>
-            //    {
-            //        opt.Endpoint = endpoint;
-            //        opt.Protocol = protocol;
-            //        opt.ExportProcessorType = OpenTelemetry.ExportProcessorType.Batch;
+            builder.AddOpenTelemetry((OpenTelemetryLoggerOptions loggingbuilder) =>
+            {
+                loggingbuilder.AddOtlpExporter(opt =>
+                {
+                    opt.Endpoint = endpoint;
+                    opt.Protocol = protocol;
+                    opt.ExportProcessorType = OpenTelemetry.ExportProcessorType.Batch;
 
-            //    }).SetResourceBuilder(
-            //        ResourceBuilder.CreateDefault()
-            //            .AddService(serviceName: config.ServiceName, serviceVersion: config.ServiceVersion));
+                }).SetResourceBuilder(
+                    ResourceBuilder.CreateDefault()
+                        .AddService(serviceName: config.ServiceName, serviceVersion: config.ServiceVersion));
 
-            //    if (config.EnableConsoleExporter)
-            //        loggingbuilder.AddConsoleExporter();
-            //});
+                if (config.EnableConsoleExporter)
+                    loggingbuilder.AddConsoleExporter();
+            });
 
             return builder;
         }
