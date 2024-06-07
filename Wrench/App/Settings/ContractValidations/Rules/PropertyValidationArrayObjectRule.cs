0661afdf3f6e9df4d3f86d59937e9503f9b69e8a
Ivan Meireles <ivanmds2@hotmail.com>
2024-06-06 21:48:58 -0300

    add file that was ignored

---
+++ Wrench/App/Settings/ContractValidations/Rules/PropertyValidationArrayObjectRule.cs
@@ -0,0 +1,25 @@
+﻿using App.Validations;
+
+namespace App.Settings.ContractValidations.Rules
+{
+    public class PropertyValidationArrayObjectRule : IRule<PropertyValidationArrayObject>
+    {
+        public ValidateResult Do(PropertyValidationArrayObject value)
+        {
+            var result = ValidateResult.Create();
+
+            if (string.IsNullOrEmpty(value.ArrayPropertyName))
+                result.AddError("PropertyValidationArrayObject.ArrayPropertyName is required");
+            else
+            {
+                if (value.ArrayPropertyName.Contains(' '))
+                    result.AddError("PropertyValidationArrayObject.ArrayPropertyName not accept empty value");
+            }
+
+            if (value.Properties == null || value.Properties.Count == 0)
+                result.AddError("PropertyValidationArrayObject.Properties is required");
+
+            return result;
+        }
+    }
+}
