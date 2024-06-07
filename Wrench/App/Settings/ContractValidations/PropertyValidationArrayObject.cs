11922205a3dff50daf86a2a38ba35e397d19c9d6
Ivan Meireles <ivanmds2@hotmail.com>
2024-06-06 21:45:39 -0300

    adjusting

---
+++ Wrench/App/Settings/ContractValidations/PropertyValidationArrayObject.cs
@@ -0,0 +1,26 @@
+﻿using App.Settings.ContractValidations.Rules;
+using App.Validations;
+
+namespace App.Settings.ContractValidations
+{
+    public class PropertyValidationArrayObject : IValidable
+    {
+        public string ArrayPropertyName { get; set; }
+        public List<PropertyValidation> Properties { get; set; }
+
+        public ValidateResult Valid()
+        {
+            var rules = new IRule<PropertyValidationArrayObject>[]
+            {
+                new PropertyValidationArrayObjectRule(),
+            };
+
+            var result = RuleExecute.Execute(this, rules);
+
+            foreach (var Property in Properties)
+                result.Concate(Property.Valid());
+
+            return result;
+        }
+    }
+}
